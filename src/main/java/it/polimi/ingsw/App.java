ee709b3314ad15cb61293227a03bc7503d8961dc
MazzaAlessandro <10670112@polimi.it>
2021-03-08 20:57:38 +0100

    test modifica

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 3;
+        int b = 2;
+        int c = a+b;
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/prova.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw;
+
+public class prova {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name = "no name";
+    int numero = 1;
+
+
+}
